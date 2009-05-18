22bba0157bfdd5712bdb3bd2f4994f994cc8442c
insideable <insideable@d1d67c14-1bda-11de-896d-b39e94f1936c>
2009-05-18 12:02:12 +0000

    Remove old trunk

--- trunk/source/core/closure.js
+++
@@ -1,6 +0,0 @@
-nulljs.module("core.closure", [], function (api) {
-	nulljs.register("core.closure",
-		function (context, f) {
-			return function () { return f.apply(context, arguments); }
-		});
-});
