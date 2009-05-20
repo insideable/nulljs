c8abde5935de21e642890ac323ccb8cdc219b7ed
insideable <insideable@d1d67c14-1bda-11de-896d-b39e94f1936c>
2009-05-20 14:51:53 +0000

    Add date plugin, allow dom.js to work with multiple documents (e.g. iframes)

--- trunk/source/plugins/dom.js
+++ trunk/source/plugins/dom.js
@@ -1,60 +1,59 @@
 nulljs.module("com.nulljs.dom", function () {
-	var D = document;
 
-	var Dom = function (tag_name, css, attributes) {
-		var e = D.createElement(tag_name);
-		css && (e.setAttribute("class", css), (e.className = css));
-		attributes && (function () { for (var i in attributes) { e.setAttribute(i, attributes[i]); } })();
-		return e;
-	};
-
-	Dom.bindTo = function (doc) {
-		return (D = doc || document, Dom);
-	};
-
-	Dom.id = function (id) {
-		return D.getElementById(id);
-	};
-
-	Dom.clear = function (element) {
-		return (element.innerHTML = "", element);
-	};
-
-	Dom.text = function (string) {
-		return D.createTextNode(string);
-	};
-
-	Dom.append = function (/* element */) {
-		var element = arguments[0], i = 0;
-		while(++i < arguments.length) {
-			arguments[i] instanceof Array ? Dom.appendList(element, arguments[i]) : element.appendChild(arguments[i]);
-//			element.appendChild(arguments[i]);
-		}
-		return element;
-	};
-
-	Dom.appendList = function (element, list) {
-//		return nulljs.dom.append.apply(element, list);
+	var appendList = function (element, list) {
 		for(var i = 0; i < list.length; i++) {
-			list[i] instanceof Array ? Dom.appendList(element, list[i]) : element.appendChild(list[i]);
+			list[i] instanceof Array ? appendList(element, list[i]) : element.appendChild(list[i]);
 		}
 		return element;
 	};
 
-	Dom.lookup = function (tag_name) {
-		return D.getElementsByTagName(tag_name);
-	};
+	var Dom = function (doc) {
+		var D = doc || document;
 
-	Dom.head = function () {
-		return Dom.lookup("head")[0];
-	};
+		var dom = function (tag_name, css, attributes) {
+			var e = D.createElement(tag_name);
+			css && (e.setAttribute("class", css), (e.className = css));
+			attributes && (function () { for (var i in attributes) { e.setAttribute(i, attributes[i]); } })();
+			return e;
+		};
 
-	Dom.body = function () {
-		return Dom.lookup("body")[0];
-	};
+		dom.id = function (id) {
+			return D.getElementById(id);
+		};
 
-	Dom.script = function (url) {
-		return Dom("script", "", { src: url });
+		dom.clear = function (element) {
+			return (element.innerHTML = "", element);
+		};
+
+		dom.text = function (string) {
+			return D.createTextNode(string);
+		};
+
+		dom.append = function (/* element */) {
+			var element = arguments[0], i = 0;
+			while(++i < arguments.length) {
+				arguments[i] instanceof Array ? appendList(element, arguments[i]) : element.appendChild(arguments[i]);
+			}
+			return element;
+		};
+
+		dom.lookup = function (tag_name) {
+			return D.getElementsByTagName(tag_name);
+		};
+
+		dom.head = function () {
+			return this.lookup("head")[0];
+		};
+
+		dom.body = function () {
+			return this.lookup("body")[0];
+		};
+
+		dom.script = function (url) {
+			return this("script", "", { src: url });
+		};
+
+		return dom;
 	};
 
 	return Dom;
